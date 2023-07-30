bc6b0727b1f7a35896b5f89e3554fed6ca1a5c43
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2023-07-30 17:54:31 -0500

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+// Test 1
+const main = document.getElementById("main");
+main.remove();
+
+// Tests 2 - 4
+const newHeader = document.createElement("h1");
+newHeader.textContent = "Adnan is the champion";
+newHeader.id = "victory";
+document.body.append(newHeader);
